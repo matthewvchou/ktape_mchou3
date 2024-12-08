6f88f90937bc3707359ddc6bc564a2de19fac776
Matthew Chou <mchou3@nd.edu>
2024-12-08 17:28:52 -0500

    Transitions Added

--- code/ktapesim_mchou3.py
+++ code/ktapesim_mchou3.py
@@ -3,6 +3,7 @@
 import sys
 import time
 import csv
+from itertools import product
 
 '''
 
@@ -22,9 +23,8 @@ TAPE_MOVEMENTS = {  'L': -1,
 # ktape Machine Class
 
 class ktape:
-    def __init__(self, name: str, num_tapes: int, tape: str, Q: str, start: str, accept: str):
+    def __init__(self, name: str, num_tapes: int, tape: str, Q: list[str], start: str, accept: list[str]):
         '''
-
         Initialization of ktape Object.
 
         Input:
@@ -34,53 +34,64 @@ class ktape:
             Q:              Set of All States
             start:          Starting State
             accept:         Set of All Accept States (Rest of States Considered Reject States)
-
         '''
         # k-Tape Machine Metadata
         self.name = name                                                                # Tape Name
         self.num_tapes = num_tapes                                                      # Number of Tapes (k)
-        self.tapes = [char for char in tape] + [['_'] for _ in range(num_tapes - 1)]    # Tapes (First contains initial input, rest initalize as blank)
+        self.tapes = [[char for char in tape]] + [['_'] for _ in range(num_tapes - 1)]  # Tapes (First contains initial input, rest initalize as blank)
         self.heads = [0 for _ in range(num_tapes)]                                      # Tape Head Positions (All initialized to 0)
+        self.current_state = start                                                      # Current State
         
         # Formal Definition
         # 7-tuple version {Q, SIGMA, GAMMA, sigma, q0, qaccept, qreject}
-        self.Q = set(Q.split(','))                                                      # Set of States
+        self.Q = set(Q)                                                                 # Set of States
         self.input_alphabet = set(char for tape in self.tapes for char in tape)         # Input Alphabet (Not Containing Blank Symbol _)
         if '*' not in self.input_alphabet:
             self.input_alphabet.add('*')
         self.tape_alphabet = self.input_alphabet.union({'_'})                           # Tape Alphabet (Containing Blank Symbol _)
         self.transitions = {}                                                           # Transitions (Will Get in Separate Function)
-        self.start = start
-        self.accept = accept.split(',')                                                 # Accept States (Assume All Other States are Reject States)
-    
-    def _make_transitions(self, ):
-        print("test")
-    
-    # def _move(tape_num: int, move: str):
+        self.start = start                                                              # Starting State
+        self.accept = accept                                                            # Accept States (Assume All Other States are Reject States)
 
-    def _print(self):
-        print(f"Name: {self.name}")
-        print(f"Number of Tapes: {self.num_tapes}")
-        print(f"List of Tapes: {self.tapes}")
-        print(f"Head Positions: {self.heads}")
-        print(f"Set of States: {self.Q}")
-        print(f"Input Alphabet: {self.input_alphabet}")
-        print(f"Tape Alphabet: {self.tape_alphabet}")
-        print(f"Starting State: {self.start}")
-        print(f"Acceptint States: {self.accept}")
+    def _simulate(self):
+        print(f"Machine Name: {self.name}")
+        print(f"Input String: {''.join(self.tapes[0])}")
 
+    def _replace_wildcards(self, tape_list):
+        # Create new tape lists with characters in tape alphabet applied to all wildcards
+        for char in self.tape_alphabet:
+            if char == '*':
+                continue
+            new_tape = [char if value == '*' else value for value in tape_list]
+            yield new_tape
 
+    def _make_transition(self, raw: list[str]):
+        # Look to see if wildcard appears
+        if '*' in raw:
+            for replacement in self._replace_wildcards(raw):
+                self._make_transition(replacement)
+        else:
+            # Splicing the list into appropriate tuples for dictionary entry
+            self.transitions[(raw[0], tuple(raw[1:self.num_tapes + 1]))] = (raw[self.num_tapes + 1], tuple(raw[self.num_tapes + 2:self.num_tapes * 2 + 2]), tuple(raw[self.num_tapes * 2 + 2:]))
+
+# Main
 def main():
-    test = ktape('test', 3, 'ababababaababa', 'q0,q1,q2,q3,q4', 'q0', 'qaccept')
-    test._print()
-
     # Check if a filenames are passed as argument
-    if len(sys.argv) < 3:
-        print("Usage: python3 knapsack_solver_mchou3.py <input.csv> <output.csv>")
+    if len(sys.argv) < 2:
+        print("Usage: python3 ktapesim_mchou3.py <full path to input.csv>")
         sys.exit(1)
 
-    # Read in CSV file and initialize TARGET and COINS for each line
+    # Read in CSV file and initialize k-tape machine
     filename = sys.argv[1]
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        # First 5 rows: Machine Name, Number of Tapes, Tape Input, Set of States, Starting State, Accepting States
+        first_five = [next(reader) for _ in range(5)]
+        machine = ktape(first_five[0][0], int(first_five[0][1]), first_five[1][0], first_five[2], first_five[3][0], first_five[4])
+        # For the rest of the rows in CSV file, should be 
+        for raw in reader:
+            machine._make_transition(raw)
+        machine._simulate()
 
 if __name__ == '__main__':
     main()
